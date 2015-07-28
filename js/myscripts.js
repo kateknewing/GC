0931da8098f6e15f28c4ad2fb5ca305943c84dac
Kate Nguyen <kate.vy.nguyen@gmail.com>
2015-07-28 15:07:05 -0400

    here

--- js/myscripts.js
+++ js/myscripts.js
@@ -16,3 +16,20 @@ $(document).ready(function(){
 });
 
 
+$(document).ready(function(){
+  $("[rel='tooltip']").tooltip();    
+ 
+    $('.thumbnail').hover(
+        function(){
+            $(this).find('.caption2').slideDown(350); //.fadeIn(250)
+        },
+        function(){
+            $(this).find('.caption2').slideUp(350); //.fadeOut(205)
+        }
+    );  
+
+  
+});
+
+
+
